f3817032c365b2eee7d24c96d3a6fb154fa703d0
Heflain da R. Mendes <heflain@outlook.com.br>
2023-11-09 22:48:41 -0300

    coreções de bug

--- tanque0.pl
+++ tanque0.pl
@@ -27,13 +27,18 @@
 %     BOOM is 1.
 
 %Nomeclatura
-%Sentido Frente ou Ré
-%Direção Direita ou Esquerda
-%Trajeto Sentido e direção
+%Sentido FORWARD, REVERSE
+%Direção  LEFT, RIGHT
 
 %controles comums
-%frente, ré, esquerda, direita, tiro
+%FORWARD, REVERSE, LEFT, RIGHT, BOOM
 movimentoValido([FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :- FORWARD =\= REVERSE, LEFT =\= RIGHT, !.
+
+%comparação de lista
+listas_iguais([], []).
+listas_iguais([H1|T1], [H2|T2]) :-
+    H1 =:= H2,
+    listas_iguais(T1, T2).
 
 %estados anterior
 entradaAnterior([0, 0, 0, 0, 0, 0, 0, 0, 0, 100]).
@@ -41,33 +46,50 @@ comandoAnterior([1, 0, 1, 0, 0]).
 movimentoRepetido(0).
 
 %Atalizando estados
-%atualizar_estados(_, Comandos) :- .
+atualizar_estados_movimentoRepetido(Comandos) :- 
+    comandoAnterior(Lc),
+    movimentoRepetido(X),
+    listas_iguais(Comandos, Lc),
+    retract(movimentoRepetido(_)),
+    Xi is X + 1,
+    asserta(movimentoRepetido(Xi)),
+    !
+    .
+atualizar_estados_movimentoRepetido( _) :- 
+    retract(movimentoRepetido(_)),
+    asserta(movimentoRepetido(0))
+    .
 
 atualizar_estados(Entradas, Comandos) :- 
+    atualizar_estados_movimentoRepetido(Comandos),
     retract(entradaAnterior(_)),
     retract(comandoAnterior(_)),
     asserta(entradaAnterior(Entradas)),
     asserta(comandoAnterior(Comandos)).
 
-%Verificando se algum carro bateu nele e o sensor não captou
+%Verificando se algum tanque bateu nele e o sensor não captou
 batida(SCORE) :- (entradaAnterior([_, _, _, _, _, _, _, _, _, ScoreAnterior]), SCORE =:= ScoreAnterior + 2).
 
 %Escolhas
 %Escolha de sentido
 %Retorno [Frente, Ré]
-escolher_sentido([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [1, 0]) :- batida(SCORE), S1 =\= 0, S2 =\= 0, S3 =\= 0, S4 =\= 0, S5 =\= 0, S6 =\= 0, !.
-escolher_sentido([_, _, _, _, _, 1, _, _, 1, _], [F , R]) :- comandoAnterior([F, R, _, _, _]), !.
-escolher_sentido([_, _, _, _, _, F, _, _, T, _], [1 , 0]) :- F =< T - 0.2, !.
+escolher_sentido([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [1, 0]) :- 
+    batida(SCORE), 
+    S1 =\= 0, S2 =\= 0, S3 =\= 0, S4 =\= 0, S5 =\= 0, S6 =\= 0.
+escolher_sentido([_, _, _, _, _, 1, _, _, 1, _], [F , R]) :- 
+    comandoAnterior([F, R, _, _, _]).
+escolher_sentido([_, _, _, _, _, F, _, _, T, _], [1 , 0]) :- 
+    F =< (T + 0.3).
 escolher_sentido(_, [0 , 1]).
 
 %Escolhas de direção
 %Retorno [Esquerda, Direita]
-escolher_direcao([_, _, _, 1, 1, _, 1, 1, _, _], [E, D]) :- comandoAnterior([_, _, E, D, _]), !.
-escolher_direcao([_, _, _, E1, E2, _, D1, D2, _, _], [1, 0]) :- ((E1 + E2) / 2) =< ((D1 + D2) / 2), !.
+escolher_direcao([_, _, _, 1, 1, _, 1, 1, _, _], [E, D]) :- comandoAnterior([_, _, E, D, _]).
+escolher_direcao([_, _, _, E1, E2, _, D1, D2, _, _], [1, 0]) :- ((E1 + E2) / 2) =< ((D1 + D2) / 2).
 escolher_direcao(_, [0, 1]).
 
 %Identifica necessidade de atirar
-identifica_necessidade_atirar([_, _, _, _, _, T, _, _, _, _], 1) :- T < 0.7, comandoAnterior([_, _, _, _, 0]), !.
+identifica_necessidade_atirar([_, _, _, _, _, T, _, _, _, _], 1) :- T < 0.6, comandoAnterior([_, _, _, _, 0]), !.
 identifica_necessidade_atirar(_, 0).
 
 %%% Faça seu codigo a partir daqui, sendo necessario sempre ter o predicado:
@@ -81,6 +103,8 @@ obter_controles([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RI
     identifica_necessidade_atirar([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], BOOM),
     movimentoValido([FORWARD, REVERSE, LEFT, RIGHT, BOOM]),
     atualizar_estados([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RIGHT, BOOM]),
+    movimentoRepetido(Qtd),
+    Qtd =< 5,
     !.
 
 obter_controles(_, [FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :- comandoAnterior([FORWARD, REVERSE, LEFT, RIGHT, BOOM]), !.
--- tanque1.pl
+++ tanque1.pl
@@ -1,7 +1,8 @@
-% Tanque 1
-:- module(tanque1, [obter_controles/2]).
+% Tanque 0
+:- module(tanque0, [obter_controles/2]).
 :- dynamic entradaAnterior/1.
 :- dynamic comandoAnterior/1.
+:- dynamic movimentoRepetido/1.
 
 %% Explicação:
 % Sensores:
@@ -26,57 +27,74 @@
 %     BOOM is 1.
 
 %Nomeclatura
-%Sentido Frente ou Ré
-%Direção Direita ou Esquerda
-%Trajeto Sentido e direção
+%Sentido FORWARD, REVERSE
+%Direção  LEFT, RIGHT
 
 %controles comums
-movimentoValido([1, 0, 0, 0, 0]).
-movimentoValido([1, 0, 0, 1, 0]).
-movimentoValido([1, 0, 1, 0, 0]).
-movimentoValido([0, 1, 0, 0, 0]).
-movimentoValido([0, 1, 0, 1, 0]).
-movimentoValido([0, 1, 1, 0, 0]).
-movimentoValido([1, 0, 0, 0, 1]).
-movimentoValido([1, 0, 0, 1, 1]).
-movimentoValido([1, 0, 1, 0, 1]).
-movimentoValido([0, 1, 0, 0, 1]).
-movimentoValido([0, 1, 0, 1, 1]).
-movimentoValido([0, 1, 1, 0, 1]).
-movimentoValido([0, 0, 0, 0, 1]).
-movimentoValido([0, 0, 0, 0, 0]).
+%FORWARD, REVERSE, LEFT, RIGHT, BOOM
+movimentoValido([FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :- FORWARD =\= REVERSE, LEFT =\= RIGHT, !.
+
+%comparação de lista
+listas_iguais([], []).
+listas_iguais([H1|T1], [H2|T2]) :-
+    H1 =:= H2,
+    listas_iguais(T1, T2).
 
 %estados anterior
 entradaAnterior([0, 0, 0, 0, 0, 0, 0, 0, 0, 100]).
-comandoAnterior([0, 0, 0, 0, 0]).
+comandoAnterior([1, 0, 1, 0, 0]).
+movimentoRepetido(0).
 
 %Atalizando estados
+atualizar_estados_movimentoRepetido(Comandos) :- 
+    comandoAnterior(Lc),
+    movimentoRepetido(X),
+    listas_iguais(Comandos, Lc),
+    retract(movimentoRepetido(_)),
+    Xi is X + 1,
+    asserta(movimentoRepetido(Xi)),
+    !
+    .
+atualizar_estados_movimentoRepetido( _) :- 
+    retract(movimentoRepetido(_)),
+    asserta(movimentoRepetido(0))
+    .
+
 atualizar_estados(Entradas, Comandos) :- 
+    atualizar_estados_movimentoRepetido(Comandos),
     retract(entradaAnterior(_)),
     retract(comandoAnterior(_)),
     asserta(entradaAnterior(Entradas)),
     asserta(comandoAnterior(Comandos)).
 
+%Verificando se algum tanque bateu nele e o sensor não captou
+batida(SCORE) :- (entradaAnterior([_, _, _, _, _, _, _, _, _, ScoreAnterior]), SCORE =:= ScoreAnterior + 2).
+
 %Escolhas
 %Escolha de sentido
-%Retorno [Frentre, Re]
-escolher_sentido([_, _, _, _, _, 1, _, _, 1, _], [F , R]) :- comandoAnterior([F, R, _, _, _]), F =\= R, !.
-escolher_sentido([_, _, _, _, _, F, _, _, T, _], [1 , 0]) :- F >= T, !.
-escolher_sentido(_, [0 , 1]) :- !.
+%Retorno [Frente, Ré]
+escolher_sentido([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [1, 0]) :- 
+    batida(SCORE), 
+    S1 =\= 0, S2 =\= 0, S3 =\= 0, S4 =\= 0, S5 =\= 0, S6 =\= 0.
+escolher_sentido([_, _, _, _, _, 1, _, _, 1, _], [F , R]) :- 
+    comandoAnterior([F, R, _, _, _]).
+escolher_sentido([_, _, _, _, _, F, _, _, T, _], [1 , 0]) :- 
+    F =< (T + 0.3).
+escolher_sentido(_, [0 , 1]).
 
 %Escolhas de direção
-%Retorno [Direita, Esquerda]
-escolher_direcao([_, _, _, 1, 1 ,_, 1, 1, _, _], [D, E]) :- comandoAnterior([_, _, D, E, _]), D =\= E, !.
-escolher_direcao([_, _, _, E1, E2 ,_, D1, D2, _, _], [1, 0]) :- (E1 + E2) / 2 >= (D1 + D2) / 2, !.
+%Retorno [Esquerda, Direita]
+escolher_direcao([_, _, _, 1, 1, _, 1, 1, _, _], [E, D]) :- comandoAnterior([_, _, E, D, _]).
+escolher_direcao([_, _, _, E1, E2, _, D1, D2, _, _], [1, 0]) :- ((E1 + E2) / 2) =< ((D1 + D2) / 2).
 escolher_direcao(_, [0, 1]).
 
 %Identifica necessidade de atirar
-identifica_necessidade_atirar([_, _, _, _, _ ,T, _, _, _, _], 1) :- T < 0.7, comandoAnterior([_, _, _, _, 0]), !.
+identifica_necessidade_atirar([_, _, _, _, _, T, _, _, _, _], 1) :- T < 0.6, comandoAnterior([_, _, _, _, 0]), !.
 identifica_necessidade_atirar(_, 0).
 
 %%% Faça seu codigo a partir daqui, sendo necessario sempre ter o predicado:
 %%%% obter_controles([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :- ...
-troca(0, 1) :- !.
+troca(0, 1).
 troca(1, 0).
 
 obter_controles([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :-
@@ -85,6 +103,8 @@ obter_controles([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RI
     identifica_necessidade_atirar([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], BOOM),
     movimentoValido([FORWARD, REVERSE, LEFT, RIGHT, BOOM]),
     atualizar_estados([X,Y,ANGLE,S1,S2,S3,S4,S5,S6,SCORE], [FORWARD, REVERSE, LEFT, RIGHT, BOOM]),
+    movimentoRepetido(Qtd),
+    Qtd =< 5,
     !.
 
 obter_controles(_, [FORWARD, REVERSE, LEFT, RIGHT, BOOM]) :- comandoAnterior([FORWARD, REVERSE, LEFT, RIGHT, BOOM]), !.
